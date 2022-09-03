e01c3388b2cbf7229c6af5b8e1754bded273402f
EnderDev <kieran@dothq.org>
2022-09-03 21:24:05 +0100

    🐛 Fix global declarations

--- index.d.ts
+++ index.d.ts
@@ -36,91 +36,93 @@ interface PathUtilsInterface {
     isAbsolute: (path: string) => boolean;
 }
 
-declare var ChromeUtils: Gecko.ChromeUtils;
+declare global {
+    var ChromeUtils: Gecko.ChromeUtils;
 
-declare var PathUtils: PathUtilsInterface;
+    var PathUtils: PathUtilsInterface;
 
-// These global objects can be used directly in JSM files only.
-// In a CommonJS context you need to import them with `require("chrome")`.
-declare var Cu: Ci;
-declare var Cc: Cc;
-declare var Ci: Ci;
-declare var Services: Gecko.Services;
+    // These global objects can be used directly in JSM files only.
+    // In a CommonJS context you need to import them with `require("chrome")`.
+    var Cu: Ci;
+    var Cc: Cc;
+    var Ci: Ci;
+    var Services: Gecko.Services;
 
-// Global actor modules
-declare class JSWindowActorParent extends Gecko.JSWindowActorParent { }
-declare class JSWindowActorChild extends Gecko.JSWindowActorChild { }
-
-/**
- * This is a variant on the normal Document, as it contains chrome-specific properties.
- */
-declare interface ChromeDocument extends Document {
-    /**
-     * Create a XUL element of a specific type. Right now this function
-     * only refines iframes, but more tags could be added.
-     */
-    createXULElement: ((type: "iframe") => XULIframeElement) &
-    ((type: string) => XULElement);
+    // Global actor modules
+    class JSWindowActorParent extends Gecko.JSWindowActorParent { }
+    class JSWindowActorChild extends Gecko.JSWindowActorChild { }
 
     /**
-     * This is a fluent instance connected to this document.
+     * This is a variant on the normal Document, as it contains chrome-specific properties.
      */
-    l10n: Gecko.FluentLocalization;
+    interface ChromeDocument extends Document {
+        /**
+         * Create a XUL element of a specific type. Right now this function
+         * only refines iframes, but more tags could be added.
+         */
+        createXULElement: ((type: "iframe") => XULIframeElement) &
+        ((type: string) => XULElement);
+
+        /**
+         * This is a fluent instance connected to this document.
+         */
+        l10n: Gecko.FluentLocalization;
+    }
+
+    /**
+     * This is a variant on the HTMLElement, as it contains chrome-specific properties.
+     */
+    interface ChromeHTMLElement extends HTMLElement {
+        ownerDocument: ChromeDocument;
+    }
+
+    interface XULElement extends HTMLElement {
+        ownerDocument: ChromeDocument;
+    }
+
+    interface XULIframeElement extends XULElement {
+        contentWindow: ChromeWindow;
+        src: string;
+    }
+
+    interface ChromeWindow extends Window {
+        openWebLinkIn: (
+            url: string,
+            where: "current" | "tab" | "tabshifted" | "window" | "save",
+            // TS-TODO
+            params?: unknown
+        ) => void;
+        openTrustedLinkIn: (
+            url: string,
+            where: "current" | "tab" | "tabshifted" | "window" | "save",
+            // TS-TODO
+            params?: unknown
+        ) => void;
+    }
+
+    class ChromeWorker extends Worker { }
+
+    interface MenuListElement extends XULElement {
+        value: string;
+        disabled: boolean;
+    }
+
+    interface XULCommandEvent extends Event {
+        target: XULElement;
+    }
+
+    interface XULElementWithCommandHandler {
+        addEventListener: (
+            type: "command",
+            handler: (event: XULCommandEvent) => void,
+            isCapture?: boolean
+        ) => void;
+        removeEventListener: (
+            type: "command",
+            handler: (event: XULCommandEvent) => void,
+            isCapture?: boolean
+        ) => void;
+    }
+
+    type nsIPrefBranch = Gecko.nsIPrefBranch;
 }
-
-/**
- * This is a variant on the HTMLElement, as it contains chrome-specific properties.
- */
-declare interface ChromeHTMLElement extends HTMLElement {
-    ownerDocument: ChromeDocument;
-}
-
-declare interface XULElement extends HTMLElement {
-    ownerDocument: ChromeDocument;
-}
-
-declare interface XULIframeElement extends XULElement {
-    contentWindow: ChromeWindow;
-    src: string;
-}
-
-declare interface ChromeWindow extends Window {
-    openWebLinkIn: (
-        url: string,
-        where: "current" | "tab" | "tabshifted" | "window" | "save",
-        // TS-TODO
-        params?: unknown
-    ) => void;
-    openTrustedLinkIn: (
-        url: string,
-        where: "current" | "tab" | "tabshifted" | "window" | "save",
-        // TS-TODO
-        params?: unknown
-    ) => void;
-}
-
-declare class ChromeWorker extends Worker { }
-
-declare interface MenuListElement extends XULElement {
-    value: string;
-    disabled: boolean;
-}
-
-declare interface XULCommandEvent extends Event {
-    target: XULElement;
-}
-
-declare interface XULElementWithCommandHandler {
-    addEventListener: (
-        type: "command",
-        handler: (event: XULCommandEvent) => void,
-        isCapture?: boolean
-    ) => void;
-    removeEventListener: (
-        type: "command",
-        handler: (event: XULCommandEvent) => void,
-        isCapture?: boolean
-    ) => void;
-}
-
-declare type nsIPrefBranch = Gecko.nsIPrefBranch;
