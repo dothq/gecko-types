238a8c0478097e258b94b589bcdefa95e2e3d40e
EnderDev <kieran@dothq.org>
2022-09-26 16:00:15 +0100

    🚧 WIP: l10n types

--- lib/FluentLocalization.d.ts
+++ lib/FluentLocalization.d.ts
@@ -2,7 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+type L10nArgs = Record<string, any>;
+
 export interface FluentLocalization {
+    formatValue(id: string, args?: L10nArgs): Promise<string | null>;
+    formatValues(ids: string[], args?: L10nArgs): Promise<string[] | null[]>;
+
     /**
      * This function sets the attributes data-l10n-id and possibly data-l10n-args
      * on the element.
